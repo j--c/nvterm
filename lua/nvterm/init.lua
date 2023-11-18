f1f0cef4f00d02f72a947ab771ab7bafd692da08
j--c <johnmichaelcollins@protonmail.com>
2023-11-18 14:10:35 -0500

    added setup method, changed get_term_txt and get_cmd_txt to local funcs

--- lua/nvterm/init.lua
+++ lua/nvterm/init.lua
@@ -1,23 +1,4 @@
-local M = {}
-M.cmd_tbl = {}
-
-M.add_cmd = function(key, cmd)
-    M.cmd_tbl[key] = cmd
-end
-
-M.run = function(split, cmd)
-    local term_txt = M.get_term_txt(split)
-    if(term_txt ~= nil) then
-        vim.cmd(term_txt)
-    end
-
-    local cmd_txt = M.get_cmd_txt(cmd)
-    if(cmd_txt ~= nil) then
-        vim.cmd(cmd_txt)
-    end
-end
-
-M.get_term_txt = function(split)
+local get_term_txt = function(split)
     local split_txt = ''
     if(split == 'h') then
         split_txt = ':split | '
@@ -27,12 +8,39 @@ M.get_term_txt = function(split)
     return split_txt .. 'terminal'
 end
 
-M.get_cmd_txt = function(cmd)
-    local cmd_txt = M.cmd_tbl[cmd]
+local get_cmd_txt = function(cmd, cmd_tbl)
+    local cmd_txt = cmd_tbl[cmd]
     if(cmd_txt == nil) then
         return nil
     else
         return ':call jobsend(b:terminal_job_id, ' .. cmd_txt .. '\\n\")' 
+    end
+end
+
+local M = {}
+M.cmd_tbl = {}
+
+M.add_cmd = function(key, cmd)
+    if(M.cmd_tbl[key] == nil) then
+        M.cmd_tbl[key] = cmd
+    end
+end
+
+M.setup = function(commands)
+    for k, v in pairs(commands) do
+        M.add_cmd(k, v)
+    end
+end
+
+M.run = function(split, cmd)
+    local term_txt = get_term_txt(split)
+    if(term_txt ~= nil) then
+        vim.cmd(term_txt)
+    end
+
+    local cmd_txt = get_cmd_txt(cmd, M.cmd_tbl)
+    if(cmd_txt ~= nil) then
+        vim.cmd(cmd_txt)
     end
 end
 
