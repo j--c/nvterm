c4667531222e993dbfe36a176b608f58d7e9788a
j--c <johnmichaelcollins@protonmail.com>
2023-11-18 09:10:06 -0500

    added function called add_cmd so that commands can be added by user and are not baked into plugin

--- lua/nvterm/init.lua
+++ lua/nvterm/init.lua
@@ -1,4 +1,21 @@
 local M = {}
+M.cmd_tbl = {}
+
+M.add_cmd = function(key, cmd)
+    M.cmd_tbl[key] = cmd
+end
+
+M.run = function(split, cmd)
+    local term_txt = M.get_term_txt(split)
+    if(term_txt ~= nil) then
+        vim.cmd(term_txt)
+    end
+
+    local cmd_txt = M.get_cmd_txt(cmd)
+    if(cmd_txt ~= nil) then
+        vim.cmd(cmd_txt)
+    end
+end
 
 M.get_term_txt = function(split)
     local split_txt = ''
@@ -10,30 +27,12 @@ M.get_term_txt = function(split)
     return split_txt .. 'terminal'
 end
 
-M.get_cmd_tbl = function()
-    local cmd_tbl = {}
-    cmd_tbl['python unittest'] = '"python3 -m unittest -v\\n")'
-    cmd_tbl['start flaskapp'] = '"python3 start_flaskapp.py\\n")'
-    cmd_tbl['behave'] = '"behave -v\\n")'
-    return cmd_tbl
-end
-
 M.get_cmd_txt = function(cmd)
-    local cmd_tbl = M.get_cmd_tbl()
-    local cmd_txt = cmd_tbl[cmd]
+    local cmd_txt = M.cmd_tbl[cmd]
     if(cmd_txt == nil) then
         return nil
     else
-        return ':call jobsend(b:terminal_job_id, ' .. cmd_txt
-    end
-end
-
-M.run = function(split, cmd)
-    local term_txt = M.get_term_txt(split)
-    vim.cmd(term_txt)
-    local cmd_txt = M.get_cmd_txt(cmd)
-    if(cmd_txt ~= nil) then
-        vim.cmd(cmd_txt)
+        return ':call jobsend(b:terminal_job_id, ' .. cmd_txt .. '\\n\")' 
     end
 end
 
