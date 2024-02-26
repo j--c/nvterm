c6e5494b8d5595fca0e1a951d7c1d48bb1bedec1
j--c <johnmichaelcollins@protonmail.com>
2024-02-26 15:58:23 -0500

    complete rewrite of this file

--- lua/nvterm/init.lua
+++ lua/nvterm/init.lua
@@ -1,59 +1,78 @@
-local get_term_txt = function(split)
-    local split_txt = ''
-    if(split == 'h') then
-        split_txt = ':split | '
-    elseif(split == 'v') then
-        split_txt = ':vsplit | '
+local get_param_string_from_file = function(paramfile)
+    local file_param_string = ''
+    local f = io.open(paramfile, 'r')
+    if f then
+        file_param_string = f:read('all')
+        f:close()
     end
-    return split_txt .. 'terminal'
+    return string.gsub(file_param_string, '[\n\r]', '')
 end
 
-local get_cmd_txt = function(cmd, cmd_tbl, parms)
-    local cmd_txt = ''
 
-    if parms ~= nil then
-        cmd_txt = cmd_tbl[cmd]
-        for _, v in ipairs(parms) do
-            print(v)
-            cmd_txt = cmd_txt .. ' ' .. v
+local get_param_string_from_cmd_tbl = function(cmd_tbl, cmd_idx)
+    local cmd_param_string = ''
+    for _, v in ipairs(cmd_tbl) do
+        if v.index == cmd_idx then
+            if v.paramfile ~= nil then
+                cmd_param_string = get_param_string_from_file(v.paramfile)
+            end
         end
-    else
-        cmd_txt = cmd_tbl[cmd]
     end
-    if(cmd_txt == nil) then
-        return nil
-    else
-        return ':call jobsend(b:terminal_job_id, "' .. cmd_txt .. '\\n\")'
-    end
+    return cmd_param_string
 end
+
+
+local get_term_string = function(split)
+    local split_string = ''
+    if(split == 'h') then
+        split_string = ':split | '
+    elseif(split == 'v') then
+        split_string = ':vsplit | '
+    end
+    return split_string .. 'terminal'
+end
+
+
+local get_cmd_string = function(cmd_tbl, cmd_idx)
+    local cmd_string = ''
+    for _, v in ipairs(cmd_tbl) do
+        if v.index == cmd_idx then
+            cmd_string = v.command
+        end
+    end
+    return cmd_string
+end
+
 
 local M = {
-    ['cmds'] = {}
 }
 
-M.add_cmd = function(key, cmd)
-    if(M.cmds[key] == nil) then
-        M.cmds[key] = cmd
-    end
+
+M.setup = function(cmd_tbl)
+    M.cmd_tbl = cmd_tbl
 end
 
-M.setup = function(commands)
-    for k, v in pairs(commands) do
-        M.add_cmd(k, v)
-    end
-end
 
-M.run = function(split, cmd, parms)
-    local term_txt = get_term_txt(split)
+M.run = function(split, cmd_idx)
+    local cmd_w_param_string = ''
+    local term_txt = get_term_string(split)
+    local cmd_string = get_cmd_string(M.cmd_tbl, cmd_idx)
+    local param_string = get_param_string_from_cmd_tbl(M.cmd_tbl, cmd_idx)
+    if cmd_string ~= nil then
+        cmd_w_param_string = cmd_string
+        if param_string ~= nil then
+            cmd_w_param_string = cmd_string .. ' ' .. param_string
+        end
+    end
+
     if(term_txt ~= nil) then
         vim.cmd(term_txt)
     end
-
-    local cmd_txt = get_cmd_txt(cmd, M.cmds, parms)
-    if(cmd_txt ~= nil) then
-        --print(cmd_txt)
-        vim.cmd(cmd_txt)
+    if cmd_w_param_string ~= nil then
+        --print(cmd_w_param_string)
+        vim.cmd(':call jobsend(b:terminal_job_id, "' .. cmd_w_param_string .. '\\n\")')
     end
 end
+
 
 return M
