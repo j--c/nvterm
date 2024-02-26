9f1a8141955844daab55858681621d4dc4c53dd6
j--c <johnmichaelcollins@protonmail.com>
2024-02-25 21:26:15 -0500

    updated to take multi params

--- lua/nvterm/init.lua
+++ lua/nvterm/init.lua
@@ -8,12 +8,19 @@ local get_term_txt = function(split)
     return split_txt .. 'terminal'
 end
 
-local get_cmd_txt = function(cmd, cmd_tbl)
-    local cmd_txt = cmd_tbl[cmd]
+local get_cmd_txt = function(cmd, cmd_tbl, parms)
+    local cmd_txt = ''
+    if parms ~= nil then
+        for _, v in ipairs(parms) do
+            cmd_txt = cmd_tbl[cmd] .. ' ' .. v
+        end
+    else
+        cmd_txt = cmd_tbl[cmd]
+    end
     if(cmd_txt == nil) then
         return nil
     else
-        return ':call jobsend(b:terminal_job_id, "' .. cmd_txt .. '\\n\")' 
+        return ':call jobsend(b:terminal_job_id, "' .. cmd_txt .. '\\n\")'
     end
 end
 
@@ -33,13 +40,13 @@ M.setup = function(commands)
     end
 end
 
-M.run = function(split, cmd)
+M.run = function(split, cmd, parms)
     local term_txt = get_term_txt(split)
     if(term_txt ~= nil) then
         vim.cmd(term_txt)
     end
 
-    local cmd_txt = get_cmd_txt(cmd, M.cmds)
+    local cmd_txt = get_cmd_txt(cmd, M.cmds, parms)
     if(cmd_txt ~= nil) then
         vim.cmd(cmd_txt)
     end
